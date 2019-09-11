7bd2c57c415f73a1770665cb496217ba7f1774ff
mage <mag@localhost.localdomain>
2019-09-11 17:11:14 +0530

    Added layouts

--- app/src/main/java/com/android/urimaiadi/MainActivity.java
+++ app/src/main/java/com/android/urimaiadi/MainActivity.java
@@ -4,20 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void traffic(View view) {
-        Toast.makeText(this,"Traffic button pressed",Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "traffic: Traffic Rules Clicked!");
         Intent intent = new Intent(this,TrafficActivity.class);
         startActivity(intent);
+    }
+
+    public void politcal(View view) {
+        Log.d(TAG, "politcal: Politics clicked");
+    }
+
+    public void railway(View view) {
+    }
+
+    public void medical(View view) {
+    }
+
+    public void education(View view) {
     }
 }
---
+++ app/src/main/java/com/android/urimaiadi/educationalActivity.java
@@ -0,0 +1,14 @@
+package com.android.urimaiadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class educationalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_educational);
+    }
+}
